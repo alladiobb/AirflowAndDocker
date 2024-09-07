dfc287a02846a438139246bbf6a92075f24b7fb5
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-09-07 12:20:48 -0300

    feat: Clean and Prepare datalake

---
+++ dags_aws/data_lake__01_clean_and_prepare.py
@@ -0,0 +1,65 @@
+import os
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.models import vabiable
+from airflow.models.baseoperator import chain
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.utils.dates import days_ago
+
+DAG_ID = os.path.basename(__file__).replace(".py","")
+
+S3_BUCKET = Variable.get("data_lake_bucket")
+
+DEFAULT_ARGS = {
+    "owner": "alladio",
+    "depends_on_past": "False",
+    "retires": 0,
+    "email_on_failure": False,
+    "email_on_retry": False,
+}
+
+with DAG(
+    dag_id=DAG_ID,
+    description="Data Lake using BashOperator and AWS CLI vs. AWS Operators",
+    default_args=DEFAULT_ARGS
+    default_timeout= timedelta(minutes=5),
+    start_date=days_ago(1),
+    schedule_interval_=None,
+    tags=["data lake"],
+
+) as dag:
+    begin = DummyOperator(task_id="begin")
+
+    end = DummyOperator(task_id="end")
+
+    delete_s3_objects = BashOperator(
+        task_id="delete_s3_objects",
+        bash_command=f'aws s3 rm "s3://{s3_bucket}/bucket_name" --recursive',
+
+    )
+    
+    list_s3_objects = BashOperator(
+        task_id="list_s3_objects",
+        bash_command=f'aws s3 rm "s3://{s3_bucket}/bucket_name" --recursive',
+
+    )
+
+    delete_catalog = BashOperator(
+        task_id="delete_catalog",
+        bash_command='aws glue delete-database --name project || echo "Database project not found."',
+    )
+
+    create_catalog = BashOperator(
+        task_id="create_catalog",
+        bash_command="""aws glue create-database --database-input \
+            '{"Name": "project", "Description": "Datasets from AWS PROJECT relational database"}'""",
+    )
+
+chain(
+    begin,
+    (delete_s3_objects, delete_catalog),
+    (list_s3_objects, create_catalog),
+    end,
+)
