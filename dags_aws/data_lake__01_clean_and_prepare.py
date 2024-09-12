39798655830b5e401fdbc2d6d2701996baf781ac
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-09-12 13:15:37 -0300

    Add: Clean and Prepare

--- dags_aws/data_lake__01_clean_and_prepare.py
+++ dags_aws/data_lake__01_clean_and_prepare.py
@@ -23,7 +23,7 @@ DEFAULT_ARGS = {
 with DAG(
     dag_id=DAG_ID,
     description="Data Lake using BashOperator and AWS CLI vs. AWS Operators",
-    default_args=DEFAULT_ARGS
+    default_args=DEFAULT_ARGS,
     default_timeout= timedelta(minutes=5),
     start_date=days_ago(1),
     schedule_interval_=None,
