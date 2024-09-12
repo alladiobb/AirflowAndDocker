87560faa737edef78c6b174b07daca99c701da70
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-09-12 13:15:55 -0300

    add: Glue and Crawlers

---
+++ dags_aws/date_lake__02_run_aws_glue_crawlers_source.py
@@ -0,0 +1,54 @@
+import os
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.models.baseoperator import chain
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.amazon.aws.operators.glue_crawler import AwsGlueCrawlerOperator
+from airflow.utils.dates import days_ago
+
+DAG_ID = os.path.basename(__file__).replace(".py","")
+
+CRAWLERS = ["project_vendas_db", "project_crm_db","project_logistica_db"]
+
+DEFAUL_ARGS = {
+    "owner": "Alladio",
+    "depends_on_past": False,
+    "retries": 0,
+    "email_on_failure": False,
+    "email_on_retry:": False,
+}
+
+with DAG (
+    dag_id=DAG_ID,
+    description= "Run Aws Glue Crawlers",
+    default_args=DEFAUL_ARGS,
+    dagrun_timeout=timedelta(minutes=15),
+    start_date=days_ago(1),
+    schedule_interval=None,
+    tags=["data lake demo", "source"],
+
+) as dag:
+    begin = DummyOperator(task_id="begin")
+
+    end = DummyOperator(task_id="end")
+    
+    list_glue_tables = BashOperator(
+        task_id = "list_glue_tables"
+        bash_command="""aws glue get-tables --database-name project \
+                        --query 'TableList[].Name' --expression "source_*" \
+                        --output table""",
+    )
+
+    for crawler in CRAWLERS:
+        crawlers_run = AwsGlueCrawlerOperator(
+            task_id=f"run_{crawler}_crawler", config={"Name": crawler}
+        )
+
+        chain(
+            begin,
+            crawlers_run,
+            list_glue_tables,
+            end,
+        )
