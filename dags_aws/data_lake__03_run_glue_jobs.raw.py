89ae7cfe7dce41bc4dcbfd0afaa5dda1f43f486f
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-09-12 13:21:16 -0300

    add: Raw Jobs

---
+++ dags_aws/data_lake__03_run_glue_jobs.raw.py
@@ -0,0 +1,52 @@
+import os
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.models.baseoperator import chain
+from airflow.operators.bash import BaseOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.providers.amazon.aws.operators.glue import AwsGlueJobOperator
+from airflow.utils.dates import days_ago
+
+DAG_ID = os.path.basename(__file__).replace(".py","")
+
+TABLES = ["users","venue","category", "date", "event", "listing", "sales"]
+
+DEFAULT_ARGS = {
+    "owner":"alladio",
+    "depends_on_past":False,
+    "retries":0,
+    "email_on_failure":False,
+    "email_on_retry":False,
+}
+
+with DAG(
+    dag_ig=DAG_ID,
+    description="Run AWS Glue ETL Jobs - Source data to raw (bronze) Data",
+    default_args=DEFAULT_ARGS,
+    dagrun_timeout=days_ago(1),
+    schedule_interval=None,
+    tags=["data lake", "raw", "bronze"],
+) as dag:
+    begin = DummyOperator(task_id="begin")
+
+    end = DummyOperator(task_id="end")
+
+    list_glue_tables = BashOperator(
+        task_id="list_glue_tables",
+        bash_command="""aws glue get-tables --database-name project \
+                        --query 'TableList[].Name' --expression "raw_*" \
+                        --output table""",
+    )
+
+    for table in TABLES:
+        start_jobs_raw = AwsGlueJobOperator(
+            task_id=f"start_job_{table}_raw", job_name=f"project_public_{table}_raw"
+        )
+
+        chain(
+            begin,
+            start_jobs_raw,
+            list_glue_tables,
+            end,
+        )
