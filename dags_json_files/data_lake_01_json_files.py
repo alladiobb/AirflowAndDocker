ce4369d4ad3468d09ea5a8f6b66ab94478ad719a
Alladio Bonesso <alladio_bonesso@hotmail.com>
2024-09-23 20:25:02 -0300

    Update: test google providers

--- dags_json_files/data_lake_01_json_files.py
+++ dags_json_files/data_lake_01_json_files.py
@@ -2,8 +2,9 @@ from datetime import datetime, timedelta
 
 from airflow.decorators import dag
 from airflow.operators.empty import EmptyOperator
+from airflow.providers.google.cloud.sensors.gcs import GCSObjectsWithPrefixExistenceSensor
 from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
-
+from airflow.providers.
 from airflow.datasets import Dataset
 
 from astro import sql as aql
@@ -11,7 +12,7 @@ from astro.files import File
 from astro.constants import FileType
 from astro.sql.table import Table, Metadata
 
-
+    
 #  criada dentr do airflow para conectar direto com o diretório na nuvem
 GCS_CONN_ID = "google_cloud_default"
 BIGQUERY_COON_ID = "google_cloud_default"
@@ -39,8 +40,6 @@ def load_json_files():
 
     init = EmptyOperator(task_id="init")
 
-    with TaskGroup(group_id=""){
-        
-    }
-
-
+    with TaskGroup(group_id="Detection") as detection_group:
+        wait_for_file = GCSObjectsWithPrefixExistenceSensor(]
+        )
